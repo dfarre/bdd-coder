1b3bd39b68bed742ead01b81ab996308c1429ac6
coleopter <daniel@naturitas.es>
2019-02-20 15:34:18 +0100

    Match hyphens and dots between double quotes

--- bdd_coder/__init__.py
+++ bdd_coder/__init__.py
@@ -8,13 +8,13 @@ def get_step_sentence(step_text):
 
 
 def sentence_to_name(text):
-    return re.sub(r'[\'`]|".+"', '', text).lower().replace(' ', '_')
+    return re.sub(r'[\'`]|"[\w\-\.]+"', '', text).lower().replace(' ', '_')
 
 
 def get_step_specs(lines, steps_map=None):
     sentences = list(map(get_step_sentence, filter(None, map(str.strip, lines))))
-    input_lists = map(lambda s: re.findall(r'"(.+)"', s), sentences)
-    output_name_lists = map(lambda s: re.findall(r'`(\w+)`', s), sentences)
+    input_lists = map(lambda s: re.findall(r'"(\w+)"', s), sentences)
+    output_name_lists = map(lambda s: re.findall(r'`([\w\-\.]+)`', s), sentences)
     steps_map = steps_map or {}
     method_names = map(lambda n: steps_map.get(n, n), map(sentence_to_name, sentences))
 
